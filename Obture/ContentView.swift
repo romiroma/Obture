839bb718a794cd28b96aff8496535c91e950bbd1
Roman Andrykevych <romiroma@icloud.com>
2022-06-29 16:31:52 +0300

    Initial Commit

---
+++ Obture/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Obture
+//
+//  Created by Roman on 29.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Obture/ObtureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObtureApp.swift
+//  Obture
+//
+//  Created by Roman on 29.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ObtureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
